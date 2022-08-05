8c00d1a976b0a800ff59bd45fe600e0e92c9d6d6
yara-stef <ymhladyak@gmail.com>
2022-08-05 09:28:15 +0000

    eported files from index.js

--- powerlifting/src/App.js
+++ powerlifting/src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
-import Download from './components/download/Download';
-import Faq from './components/faq/Faq';
-import Features from './components/features/Features';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Navbar from './components/navbar/Navbar';
-import Subscribe from './components/subscribe/Subscribe';
+import { Navbar, Header, Features, Download, Subscribe, Faq, Footer } from "./components";
 
 function App() {
   return (
