68dbf09b94444596b08903bbf8d385848e1e506a
yara-stef <ymhladyak@gmail.com>
2022-08-05 09:16:54 +0000

    imported files from components

--- powerlifting/src/App.js
+++ powerlifting/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import Download from './components/download/Download';
+import Faq from './components/faq/Faq';
+import Features from './components/features/Features';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import Subscribe from './components/subscribe/Subscribe';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <main>
+      <header className='header-bg'>
+        <Navbar />
+        <Header />
+      </header>
+      <Features />
+      <Download />
+      <Subscribe />
+      <Faq />
+      <Footer />
+    </main>
   );
 }
 
