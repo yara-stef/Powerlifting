e6eb86daf366b38b795b46157ff86e08a0729a1f
yara-stef <ymhladyak@gmail.com>
2022-08-07 17:26:01 +0000

    created Button

--- powerlifting/src/components/navbar/Navbar.js
+++ powerlifting/src/components/navbar/Navbar.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import "./Navbar.css"
+import { AiOutlineBars } from 'react-icons/ai';
+import { RiCloseLine } from 'react-icons/ri';
+import Button from '../UI/button/Button';
+import '../UI/button/Button.css';
 
 const Navbar = () => {
   return (
